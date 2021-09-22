d86c47cc4a044627764b2883d6324f705e5e9a71
Andrey Kutsak <andriykutsak.ak@gmail.com>
2021-09-22 16:58:23 +0300

    create responsive version but with some bugs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 let btn = document.querySelector("#res-btn");
+let btns = document.querySelector(".btn-wraper")
 let menu = document.querySelector("#menu");
+let togleDrop = document.querySelector("#togle-dropdown");
+let DropDown = document.querySelector(".dropdown")
 btn.addEventListener("click", function() {
-    menu.classList.toggle("hide")
+    menu.classList.toggle("hide");
+    btns.classList.toggle("hide");
+});
+togleDrop.addEventListener("click", function() {
+    DropDown.classList.toggle("hide");
+    this.classList.toggle("up")
 })
