c6d53e34f4a2df51b0c7039b87fa5f4a7f803962
Andrey Kutsak <andriykutsak.ak@gmail.com>
2021-09-22 19:56:49 +0300

    remove some bugs with acesability

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@ let btn = document.querySelector("#res-btn");
 let btns = document.querySelector(".btn-wraper")
 let menu = document.querySelector("#menu");
 let togleDrop = document.querySelector("#togle-dropdown");
-let DropDown = document.querySelector(".dropdown")
+let DropDown = document.querySelector(".dropdown");
 btn.addEventListener("click", function() {
     menu.classList.toggle("hide");
     btns.classList.toggle("hide");
 });
 togleDrop.addEventListener("click", function() {
     DropDown.classList.toggle("hide");
-    this.classList.toggle("up")
+    this.classList.toggle("up");
+    if (this.classList.contains("up")) {
+        this.classList.remove("h-line");
+    } else {
+        this.classList.add("h-line");
+    }
 })
