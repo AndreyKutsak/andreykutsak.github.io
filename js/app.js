ec42f7781b3bd2f4d5af25d8fc073b38831ea21a
Andrey Kutsak <andriykutsak.ak@gmail.com>
2021-09-27 15:19:57 +0300

    remove  bugs in resposive menu

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 let btn = document.querySelector("#res-btn");
 let btns = document.querySelector(".btn-wraper")
 let menu = document.querySelector("#menu");
-let togleDrop = document.querySelector("#togle-dropdown");
+let toggleDrop = document.querySelector("#togle-dropdown");
 let DropDown = document.querySelector(".dropdown");
+
 btn.addEventListener("click", function() {
     menu.classList.toggle("hide");
-    btns.classList.toggle("hide");
-});
-togleDrop.addEventListener("click", function() {
+})
+
+toggleDrop.addEventListener("click", function() {
     DropDown.classList.toggle("hide");
     this.classList.toggle("up");
     if (this.classList.contains("up")) {
@@ -15,4 +16,4 @@ togleDrop.addEventListener("click", function() {
     } else {
         this.classList.add("h-line");
     }
-})
+});
