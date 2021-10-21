2c03bd672f16ce182bf0dc2f8dd3c4584f1ddb2a
AndriyKutsak <andriykutsak.ak@gmail.com>
2021-10-21 14:11:33 +0300

    remove bugs with responsebility

--- newLanding/js/app.js
+++ newLanding/js/app.js
@@ -1,23 +1,27 @@
 let btn = document.querySelector("#btn"),
   menu = document.querySelector("#menu-wraper"),
-  linkReg = new RegExp(/#[a-z0-9]/, "g");
-btn.addEventListener("click", function () {
-  menu.classList.toggle("hide");
-});
-
-let link = document.querySelectorAll(".head-link");
-
-link.forEach(function (k) {
-  k.addEventListener("click", function (e) {
-    e.preventDefault();
-    link.forEach(function (k) {
-      k.classList.remove("active");
-    });
-    e.target.classList.add("active");
-    let linkVal = e.target.getAttribute("href");
-    if (linkReg.test(linkVal)) {
-      let el = document.querySelector(linkVal);
-      el.scrollIntoView({ block: "center", behavior: "smooth" });
-    }
+  linkReg = new RegExp(/#[a-z0-9]/, "g"),
+  link = document.querySelectorAll(".head-link");
+function remClass(elemnts, className) {
+  link.forEach(function (k) {
+    k.classList.remove("active");
   });
+}
+document.addEventListener("click", function (e) {
+  e.preventDefault();
+  let el = e.target;
+  switch (el) {
+    //toggle clases for btn and menu
+    case el.closest("#btn") || el.closest(".slider-item"):
+      btn.classList.toggle("clicked");
+      menu.classList.toggle("hide");
+    // get data from head link and scroll for ellement
+    case el.closest(".head-link"):
+      remClass(link, "active");
+      el.classList.add("active");
+      if (linkReg.test(el.getAttribute("href"))) {
+        let targetEl = document.querySelector(el.getAttribute("href"));
+        targetEl.scrollIntoView({ block: "center", behavior: "smooth" });
+      }
+  }
 });
