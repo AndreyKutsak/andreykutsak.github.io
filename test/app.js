d7bb4d8042ebad4e4dbc44fa486164eae80ed178
AndreyKutsak <andreykutsak.aak@gmail.com>
2021-11-16 20:25:04 +0200

    add scss for freches.com

--- test/app.js
+++ test/app.js
@@ -1,8 +1,18 @@
-function pow(a, b) {
-  if (a > 0 && b > 0) {
-    return a ** b;
-  } else {
-    alert("eroor");
-  }
+let calculator = new Calculator();
+calculator.read();
+
+function Calculator() {
+  (this.read = function () {
+    this.a = parseInt(prompt("enetre first numver"));
+    this.b = parseInt(prompt("eneter second number"));
+  }),
+    (this.sum = function () {
+      return this.a + this.b;
+    }),
+    (this.mul = function () {
+      console.log(this.a + " " + this.b);
+      return this.a - this.b;
+    });
 }
-console.log(pow(3, 3));
+alert("Mul=" + calculator.mul());
+alert("Sum=" + calculator.sum());
