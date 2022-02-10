1b0c97c8a15bd6b819f9ae6fa908d2d28da218eb
AndreyKutsak <andreykutsak.aak@gmail.com>
2022-02-10 20:51:23 +0200

    add modal window

--- dvizhok/js/app.js
+++ dvizhok/js/app.js
@@ -1,18 +1,30 @@
 $(".owl-carousel").owlCarousel({
-  autoplay: true,
-  autoplayTimeout: 4000,
-  loop: true,
-  margin: 10,
-  ceneter: true,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    600: {
-      items: 3,
-    },
-    1000: {
-      items: 3,
-    },
-  },
+	autoplay: true,
+	autoplayTimeout: 4000,
+	loop: true,
+	margin: 10,
+	ceneter: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		600: {
+			items: 3,
+		},
+		1000: {
+			items: 3,
+		},
+	},
+});
+$("#orderBtn").on("click", function () {
+	$(".modal-bg").toggleClass("hide");
+});
+$(".races-item").on("click", function () {
+	$(".modal-bg").toggleClass("hide");
+});
+$(".questionnaire").on("click", function (e) {
+	e.stopPropagation();
+});
+$(".modal-bg").on("click", function (e) {
+	$(".modal-bg").toggleClass("hide");
 });
