022bc063779a174d9899be57cfcfee3c851e7a00
AndreyKutsak <andreykutsak.aak@gmail.com>
2022-03-28 21:38:06 +0300

    sa

--- test/js/app.js
+++ test/js/app.js
@@ -1,8 +1,7 @@
 let gyroscope = new Gyroscope({ frequency: 60 });
 let data = document.querySelector("#data");
 gyroscope.addEventListener("reading", (e) => {
-	data.innerText = "";
-	"Angular velocity along the X-axis " + gyroscope.x + "Angular velocity along the Y-axis " + gyroscope.y + "Angular velocity along the Z-axis " + gyroscope.z;
+	data.innerText = "Angular velocity along the X-axis " + gyroscope.x + "Angular velocity along the Y-axis " + gyroscope.y + "Angular velocity along the Z-axis " + gyroscope.z;
 	alert();
 });
 gyroscope.start();
