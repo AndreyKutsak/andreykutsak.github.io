f8750295c4cc4a2308eec1a5d4ca914651d6b176
Andrey Kutsak <andriykutsak.ak@gmail.com>
2022-06-10 22:32:28 +0300

    fix beta

--- ags/js/app.js
+++ ags/js/app.js
@@ -2,6 +2,6 @@ let text=document.querySelector("#draw");
 window.addEventListener("deviceorientation", function(e){
   console.log(e)
 text.innerHTML=`absolute ${e.absolute} <br> alpha ${e.alpha}
-  <br> betta ${e.betta} 
+  <br> betta ${e.beta} 
   <br> gamma  ${e.gamma}`
 }, true);
