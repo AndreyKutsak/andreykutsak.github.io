f902764c0d14f38a6e8b3fdc7ea8d1826259ab8e
Andrey Kutsak <andriykutsak.ak@gmail.com>
2022-06-15 23:01:23 +0300

    find some map

---
+++ en/examples/mobile/compass.js
@@ -0,0 +1,90 @@
+const map = new Map({
+  target: "map-container",
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: fromLonLat([0, 0]),
+    zoom: 2,
+  }),
+});
+
+const source = new VectorSource();
+const layer = new VectorLayer({
+  source: source,
+});
+map.addLayer(layer);
+
+navigator.geolocation.watchPosition(
+  function (pos) {
+    const coords = [pos.coords.longitude, pos.coords.latitude];
+    source.clear(true);
+    source.addFeatures([
+      new Feature(
+        circular(coords, pos.coords.accuracy).transform(
+          "EPSG:4326",
+          map.getView().getProjection(),
+        ),
+      ),
+      new Feature(new Point(fromLonLat(coords))),
+    ]);
+  },
+  function (error) {
+    alert(`ERROR: ${error.message}`);
+  },
+  {
+    enableHighAccuracy: true,
+  },
+);
+
+const locate = document.createElement("div");
+locate.className = "ol-control ol-unselectable locate";
+locate.innerHTML = '<button title="Locate me">◎</button>';
+locate.addEventListener("click", function () {
+  if (!source.isEmpty()) {
+    map.getView().fit(source.getExtent(), {
+      maxZoom: 18,
+      duration: 500,
+    });
+  }
+});
+map.addControl(
+  new Control({
+    element: locate,
+  }),
+);
+//! [style]
+const style = new Style({
+  fill: new Fill({
+    color: "rgba(0, 0, 255, 0.2)",
+  }),
+  image: new Icon({
+    src: "./data/location-heading.svg",
+    imgSize: [27, 55],
+    rotateWithView: true,
+  }),
+});
+layer.setStyle(style);
+//! [style]
+//! [kompas]
+if (
+  window.DeviceOrientationEvent &&
+  typeof DeviceOrientationEvent.requestPermission === "function"
+) {
+  locate.addEventListener("click", function () {
+    DeviceOrientationEvent.requestPermission()
+      .then(function () {
+        const compass = new Kompas();
+        compass.watch();
+        compass.on("heading", function (heading) {
+          style.getImage().setRotation((Math.PI / 180) * heading);
+        });
+      })
+      .catch(function (error) {
+        alert(`ERROR: ${error.message}`);
+      });
+  });
+}
+//! [kompas]
