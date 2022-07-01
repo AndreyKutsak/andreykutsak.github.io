88686bf37c0a3a80fb98ce0fa968e23cbb0a89e3
Andrey Kutsak <andriykutsak.ak@gmail.com>
2022-07-01 21:39:56 +0300

    add linked in

--- test/js/app.js
+++
@@ -1,6 +0,0 @@
-let gyroscope = new Gyroscope({ frequency: 60 });
-let data = document.querySelector("#data");
-gyroscope.addEventListener("reading", (e) => {
-	data.innerText = "Angular velocity along the X-axis " + gyroscope.x + "Angular velocity along the Y-axis " + gyroscope.y + "Angular velocity along the Z-axis " + gyroscope.z;
-});
-gyroscope.start();
